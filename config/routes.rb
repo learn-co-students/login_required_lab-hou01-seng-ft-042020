2d7c3a00368016928f26e3a2b48f6cac98c3fbfe
teeman60 <taiye41@gmail.com>
2020-05-24 09:32:01 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get 'secrets/new' #why is this even there?
+
+root 'application#hello'  
+get '/login' => 'sessions#new'
+post '/login' => 'sessions#create'
+post '/logout' => 'sessions#destroy'
+
+get '/secret' => 'secrets#show' #why is there no "s" in secret?
+
 end
